a942bb1f8a3ff8b868a9579b3f3e7a1d6c267938
Nathan <G00413711@atu.ie>
2024-05-01 19:48:37 +0100

    created powerpoint for IoT demo video and edited GPS code

--- GPS_working/GPS_working.ino
+++ GPS_working/GPS_working.ino
@@ -75,76 +75,78 @@ void loop()  // run over and over again
     if (!GPS.parse(GPS.lastNMEA()))  // this also sets the newNMEAreceived() flag to false
       return;                        // we can fail to parse a sentence in which case we should just wait for another
   }
-
-  // approximately every 2 seconds or so, print out the current stats
-  if (millis() - timer > 2000) {
-    timer = millis();  // reset the timer
-    Serial.print("\nTime: ");
-    if (GPS.hour < 10) { Serial.print('0'); }
-    Serial.print(GPS.hour, DEC);
-    Serial.print(':');
-    if (GPS.minute < 10) { Serial.print('0'); }
-    Serial.print(GPS.minute, DEC);
-    Serial.print(':');
-    if (GPS.seconds < 10) { Serial.print('0'); }
-    Serial.print(GPS.seconds, DEC);
-    Serial.print('.');
-    if (GPS.milliseconds < 10) {
-      Serial.print("00");
-    } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
-      Serial.print("0");
+  if (GPS.fix) {
+    // approximately every 2 seconds or so, print out the current stats
+    if (millis() - timer > 2000) {
+      timer = millis();  // reset the timer
+      Serial.print("\nTime: ");
+      if (GPS.hour < 10) { Serial.print('0'); }
+      Serial.print(GPS.hour, DEC);
+      Serial.print(':');
+      if (GPS.minute < 10) { Serial.print('0'); }
+      Serial.print(GPS.minute, DEC);
+      Serial.print(':');
+      if (GPS.seconds < 10) { Serial.print('0'); }
+      Serial.print(GPS.seconds, DEC);
+      Serial.print('.');
+      if (GPS.milliseconds < 10) {
+        Serial.print("00");
+      } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
+        Serial.print("0");
+      }
+      Serial.println(GPS.milliseconds);
+      Serial.print("Date: ");
+      Serial.print(GPS.day, DEC);
+      Serial.print('/');
+      Serial.print(GPS.month, DEC);
+      Serial.print("/20");
+      Serial.println(GPS.year, DEC);
+      Serial.print("Fix: ");
+      Serial.print((int)GPS.fix);
+      Serial.print(" quality: ");
+      Serial.println((int)GPS.fixquality);
+      if (GPS.fix) {
+        Serial.print("Location: ");
+        Serial.print(GPS.latitude, 4);
+        Serial.print(GPS.lat);
+        float fl, fl2;
+        int i = 0;
+        double lat, longi;
+        fl = GPS.longitude / 100;
+        i = fl;
+        fl2 = fl - i;
+        fl2 = fl2 / 0.6;
+        lat = fl2 + i;
+        lat = -lat;
+        Serial.print(", ");
+        Serial.print(GPS.longitude, 4);
+        Serial.println(GPS.lon);
+        Serial.print(i);
+        Serial.print(",fl:");
+        Serial.print(lat, 6);
+        Serial.print("\n");
+        fl = GPS.latitude / 100;
+        i = fl;
+        fl2 = fl - i;
+        fl2 = fl2 / 0.6;
+        longi = fl2 + i;
+        longi = longi;
+        Serial.print(i);
+        Serial.print(",fl:");
+        Serial.print(longi, 6);
+        Serial.print("\n");
+        Serial.print("Speed (knots): ");
+        Serial.println(GPS.speed);
+        Serial.print("Angle: ");
+        Serial.println(GPS.angle);
+        Serial.print("Altitude: ");
+        Serial.println(GPS.altitude);
+        Serial.print("Satellites: ");
+        Serial.println((int)GPS.satellites);
+        Serial.print("Antenna status: ");
+        Serial.println((int)GPS.antenna);
+      }
     }
-    Serial.println(GPS.milliseconds);
-    Serial.print("Date: ");
-    Serial.print(GPS.day, DEC);
-    Serial.print('/');
-    Serial.print(GPS.month, DEC);
-    Serial.print("/20");
-    Serial.println(GPS.year, DEC);
-    Serial.print("Fix: ");
-    Serial.print((int)GPS.fix);
-    Serial.print(" quality: ");
-    Serial.println((int)GPS.fixquality);
-    if (GPS.fix) {
-      Serial.print("Location: ");
-      Serial.print(GPS.latitude, 4);
-      Serial.print(GPS.lat);
-      float fl,fl2;
-      int i = 0;
-      double lat,longi;
-      fl = GPS.longitude / 100;
-      i = fl;
-      fl2 = fl - i;
-      fl2 = fl2 / 0.6;
-      lat = fl2 + i;
-      lat=-lat;
-      Serial.print(", ");
-      Serial.print(GPS.longitude, 4);
-      Serial.println(GPS.lon);
-      Serial.print(i);
-      Serial.print(",fl:");
-      Serial.print(lat, 6);
-      Serial.print("\n");
-      fl = GPS.latitude / 100;
-      i = fl;
-      fl2= fl - i;
-      fl2 = fl2 / 0.6;
-      longi = fl2 + i;
-      longi=longi;
-      Serial.print(i);
-      Serial.print(",fl:");
-      Serial.print(longi, 6);
-      Serial.print("\n");
-      Serial.print("Speed (knots): ");
-      Serial.println(GPS.speed);
-      Serial.print("Angle: ");
-      Serial.println(GPS.angle);
-      Serial.print("Altitude: ");
-      Serial.println(GPS.altitude);
-      Serial.print("Satellites: ");
-      Serial.println((int)GPS.satellites);
-      Serial.print("Antenna status: ");
-      Serial.println((int)GPS.antenna);
-    }
-  }
+  }else{Serial.println("Test");}
+  
 }
